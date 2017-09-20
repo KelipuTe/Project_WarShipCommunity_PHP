21dc9b8516a1c664c1779cfeba5a2eb0ba539f76
KelipuTe <xhy_1365@sina.com>
2017-09-20 21:04:07 +0800

    change

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Redis;
 class MasterController extends Controller
 {
     public function welcome(){
+        Redis::set('kelipute','kelipute');
+        $name = Redis::get('kelipute');
+        dd($name);
         return view('master/welcome');
     }
 
